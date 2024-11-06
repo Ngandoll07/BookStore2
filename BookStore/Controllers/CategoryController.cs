f1f3a689d38b0b0b3ff0bda26cc4f6976cc7d167
Ngandoll <22dh112324@st.huflit.edu.vn>
2024-11-06 22:12:57 +0700

    Update loaisach

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -11,11 +11,28 @@ namespace BookStore.Controllers
     {
         private DBBookStoreEntities db = new DBBookStoreEntities();
         // GET: Category
-        public ActionResult Index()
+        /*public ActionResult Index()
         {
             var categories = db.Categories.ToList(); // Replace with actual data fetching logic
             return View(categories);
+        }*/
+        public ActionResult Index(string id)
+        {
+            // Kiểm tra nếu id không rỗng và không null
+            if (!string.IsNullOrEmpty(id))
+            {
+                // Lọc các sản phẩm theo Category ID
+                var filteredBooks = db.Books.Where(b => b.Category.ID == id).ToList();
+                return View(filteredBooks);
+            }
+            else
+            {
+                // Nếu không có ID thể loại, trả về toàn bộ sản phẩm hoặc xử lý lỗi nếu cần
+                var allBooks = db.Books.ToList();
+                return View(allBooks);
+            }
         }
+
         public ActionResult Partial_Category()
         {
             var items = db.Books.Take(12).ToList();
@@ -27,7 +44,5 @@ namespace BookStore.Controllers
             var items = db.Categories.ToList();
             return PartialView("_PartialMenuCate",items); // Ensure you have a corresponding view
         }
-        
-
     }
 }
